5c607e10a378979bf451940f1e5b2651727fa1e5
ajaishakya <ajaishakya08@gmail.com>
2023-06-23 13:20:29 +0545

    first commit

---
+++ DAS/views.py
@@ -0,0 +1,117 @@
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+from DAS import email_backend
+from app.models import Patient
+
+def index(request):
+    return render(request,'main/index.html')
+
+def DOCTOR_DASHBOARD(request):
+    return render(request,'main/doctor-dashboard.html')
+
+def APPOINTMENTS(request):
+    return render(request,'main/appointments.html')
+
+def LOGIN(request):
+    return render(request,'main/login.html')
+
+def register(request):
+    if request.method == "POST":
+        fname = request.POST.get('fname')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        # check username
+        if User.objects.filter(username=username).exists():
+           messages.warning(request,'That username has already been taken!')
+           return redirect('register')
+        
+        # check email
+        if User.objects.filter(email=email).exists():
+           messages.warning(request,'Email already exists!')
+           return redirect('register')
+        
+        user = User(
+            first_name = fname,
+            last_name = "Patient",
+            username = username,
+            email = email,
+        )
+
+        user.set_password(password)
+        user.save()
+
+        # user_id = request.user.id
+        # user = User.objects.get(id=user_id)
+        # patient = user.patient
+        # patient.id = user_id
+
+        return redirect('login')
+    return render(request,'main/register.html')
+
+def DO_LOGIN(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = email_backend.EmailBackEnd.authenticate(request,
+                                     username=email,
+                                     password=password)
+        if user!=None:
+           login(request,user)
+           return redirect('patient-dashboard')
+        else:
+           messages.error(request,'Email and Password Are Invalid !')
+           return redirect('login')
+
+def PATIENT_DASHBOARD(request):
+    return render(request,'main/patient-dashboard.html')
+
+@login_required(login_url="/main/login/")
+def PROFILE_SETTINGS(request):
+    if request.method == "POST":
+        image = request.POST.get('image')
+        username = request.POST.get('username')
+        fname = request.POST.get('fname')
+        email = request.POST.get('email')
+        blood = request.POST.get('blood')
+        dob= request.POST.get('dob')
+        mobile = request.POST.get('mobile')
+        address = request.POST.get('address')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zip = request.POST.get('zip')
+        country = request.POST.get('country')
+
+    # Important code to access user and patient table
+        user_id = request.user.id
+        user = User.objects.get(id=user_id)
+        patient = user.patient
+        
+    # To update existing records
+        patient.profile_pic = image
+        patient.dob = dob
+        patient.blood_group = blood
+        patient.mobile = mobile
+        patient.address = address
+        patient.city = city
+        patient.state = state
+        patient.zip_code = zip
+        patient.country = country
+        
+        patient.save()
+
+        user.first_name = fname
+        user.username = username
+        user.email = email
+            
+        user.save()
+
+        return render(request,'main/profile-settings.html')
+    return render(request,'main/profile-settings.html')
---
+++ app/models.py
@@ -0,0 +1,54 @@
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+# Create your models here.
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Add your custom fields here
+    profile_pic = models.ImageField(upload_to="media/patient",null=True) #stored in a separate media folder
+    dob = models.DateField(null=True)
+    blood_group = models.CharField(max_length=3,null=True)
+    mobile = models.CharField(max_length=15 ,null=True)
+    address = models.CharField(max_length=150 ,null=True)
+    city = models.CharField(max_length=50 ,null=True)
+    state = models.CharField(max_length=50 ,null=True)
+    zip_code = models.CharField(max_length=10 ,null=True)
+    country = models.CharField(max_length=100 ,null=True)
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Add your custom fields here
+    profile_pic = models.ImageField(upload_to="media/doctor",null=True)
+    mobile = models.CharField(max_length=15,null=True)
+    dob = models.DateField(null=True)
+    bio = models.TextField(blank=True)
+    address = models.CharField(max_length=150,null=True)
+    pricing = models.IntegerField(null=True)
+    degree = models.CharField(max_length=10 ,null=True)
+    experience = models.CharField(max_length=5 ,null=True)
+    designation = models.CharField(max_length=30 ,null=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        if instance.last_name == "Patient":
+            Patient.objects.create(user=instance)
+
+        elif instance.last_name == "Doctor":
+            Doctor.objects.create(user=instance)
+        
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    if instance.last_name == "Patient":
+        instance.patient.save()
+
+    elif instance.last_name == "Doctor":
+        instance.doctor.save()
+
+
+
+
